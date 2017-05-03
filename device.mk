c6b056726cf2a898520b3b3160e4e1d8fd599348
Dan Pasanen <dan.pasanen@gmail.com>
2017-05-03 10:00:22 -0500

    taimen: set ui thread runnable time to zero
    * https://source.android.com/devices/tech/debug/jank_jitter#sys.use_fifo_ui
    
    Change-Id: Idc7723e14b3e3920d5736589dcf44e5525d10518

--- device.mk
+++ device.mk
@@ -158,3 +158,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # ModemService
 PRODUCT_COPY_FILES += \
   device/google/taimen/whitelist_modemservice.xml:system/etc/sysconfig/whitelist_modemservice.xml
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
