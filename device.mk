aaf5c163eb3a25a716aed20b71cd9b21d83bef17
Christian Oder <myself5@carbonrom.org>
2018-04-01 18:14:23 +0200

    taimen: Exclude ModemService from powersave
    * Fixes FC of ModemService caused by nonallowed intents while background mode.
    
    Change-Id: I8759e1a21bb1bbcf402fb4b52c9335e6f8d6bef5

--- device.mk
+++ device.mk
@@ -154,3 +154,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.data_ltd_sys_ind=1 \
     persist.radio.data_con_rprt=1 \
     persist.radio.calls.on.ims=1
+
+# ModemService
+PRODUCT_COPY_FILES += \
+  device/google/taimen/whitelist_modemservice.xml:system/etc/sysconfig/whitelist_modemservice.xml
